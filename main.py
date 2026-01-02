27dd23113437242f8e61c37cc149db8c4fa76f0e
Utku <balikcilar.utku@gmail.com>
2026-01-02 14:37:12 +0300

    Main before calculations

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("pyCharm")
+def gridOlustur(dx, lenght=1.0):
+    adet = int(round(lenght/dx))
+    xList = []
+    for i in range(adet + 1):
+        xList.append(i * dx)
+    return xList
+
+def main():
+    dx = 0.1
+    timeInput = input("Zaman değerini giriniz* : ").strip()
+    if timeInput == "":
+        print("Zaman değeri boş olamaz.")
+        return
+    t = float(timeInput)
+
+    locInput = input("Konum değerini giriniz : ").strip()
+
+    if locInput == "":
+        print("TABLO")
+        print("t :", t)
+        xList = gridOlustur(dx, 1.0)
+        print("x noktaları :", xList)
+    else:
+        x = float(locInput)
+        print("TEK NOKTA")
+        print("t :", t, "x :", x)
+
+if __name__ == "__main__":
+    main()
